3ce120c92fc53d9c4631e71dffa24a99487c8380
mfried95 <m.friedman1995@gmail.com>
2023-04-26 16:57:00 -0400

    Completed all tasks with Ajax and validation/ error messages

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,58 +5,24 @@
  */
 
 // Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  user: {
-    name: "Newton",
-    avatars: "https://i.imgur.com/73hZDYK.png",
-    handle: "@SirIsaac",
-  },
-  content: {
-    text: "If I have seen further it is by standing on the shoulders of giants",
-  },
-  createdAt: 1461116232227,
-};
 
-const render = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
-    },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants",
-    },
-    createdAt: 1682359985490,
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd",
-    },
-    content: {
-      text: "Je pense , donc je suis",
-    },
-    createdAt: 1682446385490,
-  },
-];
 
-const getCurrentDateTime = function() {
-  const now = new Date();
-  const year = now.getFullYear();
-  const month = now.getMonth() + 1;
-  const day = now.getDate();
-  const hours = now.getHours();
-  const minutes = now.getMinutes();
-  const seconds = now.getSeconds();
-  const dateTimeString = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-  return dateTimeString;
-};
+$(document).ready(() => {
+  const getCurrentDateTime = function() {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = now.getMonth() + 1;
+    const day = now.getDate();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+    const dateTimeString = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+    return dateTimeString;
+  };
 
-const createTweetElement = function(tweet) {
-  const $article = $(`<article>`).addClass("tweet-container");
-  const $tweetData = $(`
+  const createTweetElement = function(tweet) {
+    const $article = $(`<article>`).addClass("tweet-container");
+    const $tweetData = $(`
       <header>
         <img src="${tweet.user.avatars}" />
         <h3>${tweet.user.name}</h3>
@@ -74,21 +40,69 @@ const createTweetElement = function(tweet) {
         </div>
       </footer>
   `);
-  const checking = $article.append($tweetData);
+    const checking = $article.append($tweetData);
 
-  return checking;
-};
+    return checking;
+  };
 
-$(document).ready(() => {
+  const renderTweets = function(tweets) {
+    // clear existing tweets from the DOM
+    $(".tweets-section").empty();
+
+    // loop through each tweet
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $(".tweets-section").append($tweet);
+    }
+  };
+
+  const addPost = (event) => {
+    event.preventDefault(); // prevent the default submit
+    const tweetLength = $('#tweet-text').val().length;
+    const $error = $("#error-message").css("color", "red");
+
+    // check the length of the tweet content
+    if (tweetLength < 140) {
+      // serialize the form data
+      const formData = $(event.target).serialize();
+
+      // AJAX request
+      $.ajax({
+        url: "/tweets",
+        method: "POST",
+        data: formData,
+        success: () => {
+          // clear the form input and reload the tweets
+          $(event.target).find("textarea").val("");
+          loadtweets();
+        },
+        error: (error) => {
+          console.log(error);
+        },
+      });
+    } else {
+      $error.text('Error: tweet is too long!');
+      $error.color('red');
+      $error.removeClass('hidden');
+      return;
+    }
+  };
+
+  const loadtweets = () => {
+    $.ajax({
+      url: "/tweets",
+      method: "GET",
+      success: (tweets) => {
+        renderTweets(tweets);
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  };
+
   // const $tweets = createTweetElement(tweetData);
   // $(".tweets-section").append($tweets);
-  renderTweets(render);
+  loadtweets();
+  $("form").submit(addPost);
 });
-
-const renderTweets = function(tweets) {
-  // loop through each tweet
-  for (const tweet of tweets) {
-    const $tweet = createTweetElement(tweet);
-    $(".tweets-section").append($tweet);
-  }
-};
