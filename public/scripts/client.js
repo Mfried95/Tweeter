896efcbb67020545c2a88145ddf82ff80600ee4b
mfried95 <m.friedman1995@gmail.com>
2023-05-01 19:20:16 -0400

    Added changes to validation, timeago, css styling

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,65 +6,53 @@
 
 // Test / driver code (temporary). Eventually will get this from the server.
 
-
 $(document).ready(() => {
-  const getCurrentDateTime = function() {
-    const now = new Date();
-    const year = now.getFullYear();
-    const month = now.getMonth() + 1;
-    const day = now.getDate();
-    const hours = now.getHours();
-    const minutes = now.getMinutes();
-    const seconds = now.getSeconds();
-    const dateTimeString = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-    return dateTimeString;
-  };
 
   const createTweetElement = function(tweet) {
-    const $article = $(`<article>`).addClass("tweet-container");
+    const $article = $("<article>").addClass("tweet-container");
     const $tweetData = $(`
-    <section class="new-tweets-section">
-    <article id="tweet">
-      <header class="new-header">
-      <img src="${tweet.user.avatars}" class="avatar" />
-        ${tweet.user.name}
-        <span>${tweet.user.handle}</span>
-      </header>
-      <p>${tweet.content.text}</p>
-      <footer>
-      <span class="current-time">${getCurrentDateTime()}</span>
-        <div class="actions">
-            <i class="fa-solid fa-flag"></i>
-            <i class="fa-solid fa-retweet"></i>
-            <i class="fa-solid fa-heart"></i>
-        </div>
-      </footer>
-    </article>
-  </section>
-  `);
-    const checking = $article.append($tweetData);
-
-    return checking;
+      <section class="new-tweets-section">
+        <article id="tweet">
+          <header class="new-header">
+            <img src="${tweet.user.avatars}" class="avatar" />
+            ${tweet.user.name}
+            <span>${tweet.user.handle}</span>
+          </header>
+          <p>${tweet.content.text}</p>
+          <footer>
+            <span class="time-ago">${timeago.format(tweet.created_at)}</span>
+            <div class="actions">
+              <i class="fa-solid fa-flag"></i>
+              <i class="fa-solid fa-retweet"></i>
+              <i class="fa-solid fa-heart"></i>
+            </div>
+          </footer>
+        </article>
+      </section>
+    `);
+  
+    $article.append($tweetData);
+  
+    return $article;
   };
 
-  const renderTweets = function(tweets) {
+  const renderTweets = function (tweets) {
     // clear existing tweets from the DOM
-    $(".tweets-section").empty();
 
     // loop through each tweet
     for (const tweet of tweets) {
       const $tweet = createTweetElement(tweet);
-      $(".tweets-section").append($tweet);
+      $(".tweets-section").prepend($tweet);
     }
   };
 
   const addPost = (event) => {
     event.preventDefault(); // prevent the default submit
-    const tweetLength = $('#tweet-text').val().length;
+    const tweetLength = $("#tweet-text").val().length;
     const $error = $("#error-message").css("color", "red");
 
     // check the length of the tweet content
-    if (tweetLength < 140) {
+    if (tweetLength <= 140) {
       // serialize the form data
       const formData = $(event.target).serialize();
 
@@ -73,19 +61,26 @@ $(document).ready(() => {
         url: "/tweets",
         method: "POST",
         data: formData,
-        success: function() {
+        success: function () {
           // clear the form input and reload the tweets
-          $(event.target).find("textarea").val("");
+          $(event.target).find("textarea").val(""); // clear the textarea
+          $(event.target).find(".counter").text("140"); // reset the character counter to 140
           loadtweets();
         },
-        function(error) {
-          console.log(error);
+        function() {
+          $error.text("There is an error");
         },
       });
+    } else if (tweetLength === 0) {
+      // display an error message for empty tweet
+      $error.text("Error: tweet cannot be empty!");
+      $error.color("red");
+      $error.removeClass("hidden");
+      return;
     } else {
-      $error.text('Error: tweet is too long!');
-      $error.color('red');
-      $error.removeClass('hidden');
+      $error.text("Error: tweet is too long!");
+      $error.color("red");
+      $error.removeClass("hidden");
       return;
     }
   };
@@ -94,10 +89,10 @@ $(document).ready(() => {
     $.ajax({
       url: "/tweets",
       method: "GET",
-      success: function(tweets) {
+      success: function (tweets) {
         renderTweets(tweets);
       },
-      error: function(error) {
+      error: function (error) {
         console.log(error);
       },
     });
